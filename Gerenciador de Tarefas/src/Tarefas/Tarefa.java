74ccdc569a018906270fb457c48a23be18b2ebf0
Felipe F. Souza <140359342+G2Kzin@users.noreply.github.com>
2024-09-03 21:37:31 -0300

    Add files via upload

---
+++ Gerenciador de Tarefas/src/Tarefas/Tarefa.java
@@ -0,0 +1,48 @@
+package Tarefas;
+
+public class Tarefa {
+
+    private String nomeTarefa;
+    private String oqFaz;
+    private String data;
+    private boolean concluida;
+
+    public Tarefa(String nomeTarefa, String tempo) {
+        this.nomeTarefa = nomeTarefa;
+        this.data = tempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa: " + nomeTarefa + "\nTempo: " + data + "\nO que faz: " + oqFaz + "\n";
+    }
+
+    public String getNomeTarefa() {
+        return nomeTarefa;
+    }
+
+    public void setNomeTarefa(String nomeTarefa) {
+        this.nomeTarefa = nomeTarefa;
+    }
+
+    public String getOqFaz() {
+        return oqFaz;
+    }
+
+    public void setOqFaz(String oqFaz) {
+        this.oqFaz = oqFaz;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
+
